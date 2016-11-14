5b7c62ca1c8f3f60bbabb6b287fa7b9747759087
Marcin Sulecki <marcin.sulecki@gmail.com>
2016-11-14 11:30:54 +0100

    Add project files.

---
+++ Vavatech.WPF.WinFormsClient/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vavatech.WPF.WinFormsClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello World!";
+        }
+
+
+        private string firstname = "Marcin";
+
+
+        //  label1.BackgroundColor  = person.Address.City
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = firstname;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            firstname = label1.Text;
+        }
+    }
+}
