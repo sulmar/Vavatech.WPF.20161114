e9b6db8b35755aebd7af7379ec15562a6f957a43
Marcin Sulecki <marcin.sulecki@gmail.com>
2016-11-15 12:11:10 +0100

    Utworzenie interfejsu IStationsService

---
+++ Vavatech.Bicycle.Interfaces/IStationsService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vavatech.Bicycle.Models;
+
+namespace Vavatech.Bicycle.Interfaces
+{
+    public interface IStationsService
+    {
+        IList<Station> Get();
+
+        Station Get(int stationId);
+
+        void Add(Station station);
+
+        void Update(Station station);
+
+        void Remove(int stationId);
+    }
+}
