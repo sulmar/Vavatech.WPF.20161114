649b78b9b9a7debfc10896e9d719adacc78aaf2b
Marcin Sulecki <marcin.sulecki@gmail.com>
2016-11-16 12:23:30 +0100

    Utworzenie własnego MarkupExtension

---
+++ Vavatech.Bicycle.WPFClient/MarkupExtensions/CurrentDate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace Vavatech.Bicycle.WPFClient.MarkupExtensions
+{
+    public class CurrentDate : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return DateTime.Now.ToShortTimeString();
+        }
+    }
+}
