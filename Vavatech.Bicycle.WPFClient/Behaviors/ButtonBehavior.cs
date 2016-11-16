f1972507846ff61ffd4729a9e4cf8db99a89fdad
Marcin Sulecki <marcin.sulecki@gmail.com>
2016-11-16 11:39:13 +0100

    Dodanie behaviora

---
+++ Vavatech.Bicycle.WPFClient/Behaviors/ButtonBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+using System.Windows.Media;
+
+namespace Vavatech.Bicycle.WPFClient.Behaviors
+{
+    public class ButtonBehavior : Behavior<Button>
+    {
+        protected override void OnAttached()
+        {
+            var button = this.AssociatedObject;
+
+            button.MouseEnter += Button_MouseEnter;
+        }
+
+        private void Button_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var button = sender as Button;
+
+            button.Background = new SolidColorBrush(Colors.Red);
+        }
+
+    }
+}
