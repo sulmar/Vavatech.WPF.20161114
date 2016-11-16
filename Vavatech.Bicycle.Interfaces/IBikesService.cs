031501dbdb2f0e1a9bfc4092edde44d01864e88e
Marcin Sulecki <marcin.sulecki@gmail.com>
2016-11-16 08:30:59 +0100

    Utworzenie generycznej interfejsu do operacji CRUD

--- Vavatech.Bicycle.Interfaces/IBikesService.cs
+++ Vavatech.Bicycle.Interfaces/IBikesService.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Vavatech.Bicycle.Models;
 
 namespace Vavatech.Bicycle.Interfaces
 {
-    public interface IStationsService : ICrudService<Station>
+    public interface IBikesService : ICrudService<Bike>
     {
-       
     }
 }
---
+++ Vavatech.Bicycle.Interfaces/ICRUDService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vavatech.Bicycle.Interfaces
+{
+    public interface ICrudService<TItem>
+    {
+        IList<TItem> Get();
+
+        TItem Get(int itemId);
+
+        void Add(TItem item);
+
+        void Update(TItem item);
+
+        void Remove(int itemId);
+    }
+}
